559133a1f3b7a5fc78661738b9e9dbf70705f39f
Rodesigned <rodesigned@att.net>
2018-03-18 05:48:40 -0400

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Killer Burger');
+INSERT INTO burgers (burger_name) VALUES ('Incredible Hulk Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy cheese Burger', true);
