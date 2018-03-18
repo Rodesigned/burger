a6082320f3c13be67c47e732b544704ffa2440dd
Rodesigned <rodesigned@att.net>
2018-03-18 08:47:23 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV === "production") {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+    port: 3306,
     host: "localhost",
     user: "root",
     password: "Nikeo369*",
@@ -15,6 +16,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
